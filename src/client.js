f2c12172675768982ea3a9f3eac0e3d64203504a
hung.ta <hung.ta@epspot.com>
2021-05-27 12:04:40 +0200

    added tailwind styling for AllPosts / OnePost page

--- src/client.js
+++ src/client.js
@@ -1,9 +1,7 @@
-import sanityClient from "@sanity/client"
+import sanityClient from '@sanity/client';
 
-export default sanityClient(
-    {
-        projectId: "4edj4bzz",
-        dataset: "production",
-        useCdn: true,
-    }
-);
+export default sanityClient({
+  projectId: '4edj4bzz',
+  dataset: 'production',
+  useCdn: true,
+});
